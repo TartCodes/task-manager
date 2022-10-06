8a3dfb3376cda4cae2c0bd0cc080b38c1e5cd91c
TartCodes <tartcodes@gmail.com>
2022-10-06 13:45:26 -0400

    button functionality

--- src/App.js
+++ src/App.js
@@ -34,15 +34,12 @@ function App() {
       reminder: false,
     },
   ]);
-
   //add task
-
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task }
     setTasks([...tasks, newTask])
   }
-
   //delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -58,7 +55,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={ () => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,33 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({title}) => {
+const Header = ({ title, onAdd, showAdd }) => {
   return (
-    <header className= 'header'>
-        <h1>{title}</h1>
-        <Button color='green' text='Add'/>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Manager'
-}
+  title: "Task Manager",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
 //a way to style css
 //<h1 style={headingStyle}
 // const headingStyle = {
-//     color: 'red', 
+//     color: 'red',
 //     backgroundColor: 'black'
 // }
 
-export default Header
+export default Header;
